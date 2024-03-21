8d1853d419357d12ea2ec79c48ad8e6ddb38f163
kanada <kanada.masayuki.hata@gmail.com>
2024-03-21 11:41:18 +0900

    EventStore

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,6 +6,8 @@ use App\Models\Event;
 use App\Http\Requests\StoreEventRequest;
 use App\Http\Requests\UpdateEventRequest;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use App\Services\EventService;
 
 class EventController extends Controller
 {
@@ -34,7 +36,30 @@ class EventController extends Controller
      */
     public function store(StoreEventRequest $request)
     {
-        dd($request);
+        $check = EventService::checkEventDuplication(
+            $request['event_date'],$request['start_time'],$request['end_time']
+        );
+
+        if($check) {
+            session()->flash('status', 'この時間帯は既に他の予約が入っています。他の時間帯を指定して再度登録してください。');
+            return view('manager.events.create');
+        }
+
+        $startDate = EventService::joinDateAndTime($request['event_date'],$request['start_time']);
+        $endDate = EventService::joinDateAndTime($request['event_date'],$request['end_time']);
+
+        Event::create([
+            'name' => $request['event_name'],
+            'information' => $request['information'],
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'max_people' => $request['max_people'],
+            'is_visible' => $request['is_visible'],
+        ]);
+
+        session()->flash('status', '登録が完了しました。');
+
+        return to_route('events.index');
     }
 
     /**
