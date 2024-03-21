ae40775926bbc4d9ca2abe9b1fde573378d619a9
kanada <kanada.masayuki.hata@gmail.com>
2024-03-21 17:37:56 +0900

    EventUpdate

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -7,13 +7,30 @@ use Carbon\Carbon;
 
 class EventService
 {
-    public static function checkEventDuplication($eventDate, $startTime, $endTime)
+    public static function checkEventDuplicationExceptOwn($event, $eventDate, $startTime, $endTime)
+    {
+        $event = DB::table('events')
+            ->whereDate('start_date', $eventDate)
+            ->whereTime('end_date', '>', $startTime)
+            ->whereTime('start_date', '<', $endTime)
+            ->get()
+            ->toArray();
+
+        if (count($event) > 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static function countEventDuplication($eventDate, $startTime, $endTime)
     {
         return DB::table('events')
-            ->whereDate('start_date', $eventDate)
-            ->whereTime('start_date', '<', $startTime)
-            ->whereTime('end_date', '>' , $endTime)
-            ->exists();
+         ->whereDate('start_date', $eventDate)
+         ->whereTime('end_date', '>', $startTime)
+         ->whereTime('start_date', '<', $endTime)
+         ->count();
     }
 
     public static function joinDateAndTime($date, $time)
