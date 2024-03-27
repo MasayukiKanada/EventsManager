efb06db78b3cbbad3bad6cde6ba102f715ef41fc
kanada <kanada.masayuki.hata@gmail.com>
2024-03-27 18:01:20 +0900

    ReservationNumberOfPeople

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -18,7 +18,14 @@ class EventController extends Controller
     {
         $today = Carbon::today();
 
+        $reservedPeople = DB::table('reservations')
+        ->select('event_id', DB::raw('sum(number_of_people) as number_of_people'))
+        ->groupBy('event_id');
+
         $events = DB::table('events')
+        ->leftJoinSub($reservedPeople, 'reservedPeople', function($join) {
+            $join->on('events.id', '=', 'reservedPeople.event_id');
+        })
         ->whereNull('deleted_at')
         ->whereDate('start_date', '>=', $today)
         ->orderBy('start_date', 'asc')
@@ -137,7 +144,15 @@ class EventController extends Controller
     public function past()
     {
         $today = Carbon::today();
+
+        $reservedPeople = DB::table('reservations')
+        ->select('event_id', DB::raw('sum(number_of_people) as number_of_people'))
+        ->groupBy('event_id');
+
         $events = DB::table('events')
+        ->leftJoinSub($reservedPeople, 'reservedPeople', function($join) {
+            $join->on('events.id', '=', 'reservedPeople.event_id');
+        })
         ->whereNull('deleted_at')
         ->whereDate('start_date', '<', $today)
         ->orderBy('start_date', 'desc')
