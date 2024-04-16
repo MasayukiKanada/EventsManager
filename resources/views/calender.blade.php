d5b81e77feac4a6118bcc0bf30a9e24b82978864
kanada <kanada.masayuki.hata@gmail.com>
2024-04-16 13:51:14 +0900

    CalenderLayout

--- resources/views/calender.blade.php
+++ resources/views/calender.blade.php
@@ -1,24 +1,18 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<x-calender-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            イベントカレンダー
+        </h2>
+    </x-slot>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/flatpickr.js',])
-
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased">
-        @livewire('calender')
-        @livewireScripts
-    </body>
-</html>
+                <div class="max-w-2xl mx-auto py-8 px-4">
+                @livewire('calender')
+                </div>
+            </div>
+        </div>
+    </div>
+</x-calender-layout>
