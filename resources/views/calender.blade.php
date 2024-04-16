7b9070700bc5c1cdb5eba67b2b96202900df238f
kanada <kanada.masayuki.hata@gmail.com>
2024-04-16 16:05:05 +0900

    CalenderComponent

--- resources/views/calender.blade.php
+++ resources/views/calender.blade.php
@@ -5,13 +5,10 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-4">
+        <div class="event-calender border border-red-400 mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-
-                <div class="max-w-2xl mx-auto py-8 px-4">
                 @livewire('calender')
-                </div>
             </div>
         </div>
     </div>
